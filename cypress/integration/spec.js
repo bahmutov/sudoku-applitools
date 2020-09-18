c66b5d0682925956c53e7d74dc77923ac3b3682a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-18 14:03:28 -0400

    update for domain

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,5 +3,15 @@
 describe('Sudoku', () => {
   it('works', () => {
     cy.visit('/')
+    // on easy setting there are 45 filled cells at the start
+    cy.get('.game__cell--filled').should('have.length', 45)
+    cy.contains('.status__time', '00:00')
+    cy.contains('.status__difficulty-select', 'Easy')
+
+    cy.viewport(600, 800)
+    cy.get('.game__cell--filled').should('have.length', 45)
+
+    cy.viewport(300, 600)
+    cy.get('.game__cell--filled').should('have.length', 45)
   })
 })
