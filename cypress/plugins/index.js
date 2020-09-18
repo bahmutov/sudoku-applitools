3338d3236a63ea19b5ea690ece44603aefe034f9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-18 14:39:11 -0400

    overwrite eye commands if API key is not set

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,12 @@
-require('@applitools/eyes-cypress')(module)
-
 module.exports = (on, config) => {
   require('cypress-react-unit-test/plugins/react-scripts')(on, config)
   // IMPORTANT to return the config object
   // with the any changed environment variables
+  if (process.env.APPLITOOLS_API_KEY) {
+    config.env.APPLITOOLS_SETUP = '1'
+  }
+
   return config
 }
+
+require('@applitools/eyes-cypress')(module)
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,18 +1,26 @@
+/// <reference types="cypress" />
 // set up Applitools following
 // https://www.npmjs.com/package/@applitools/eyes-cypress
 import '@applitools/eyes-cypress/commands'
 require('cypress-react-unit-test/support')
 
-beforeEach(() => {
-  cy.eyesOpen({
-    appName: 'Sudoku',
-    browser: {
-      width: cy.state('viewportWidth'),
-      height: cy.state('viewportHeight'),
-    },
+if (Cypress.env('APPLITOOLS_SETUP')) {
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: 'Sudoku',
+      browser: {
+        width: cy.state('viewportWidth'),
+        height: cy.state('viewportHeight'),
+      },
+    })
   })
-})
 
-afterEach(() => {
-  cy.eyesClose()
-})
+  afterEach(() => {
+    cy.eyesClose()
+  })
+} else {
+  // do nothing for visual test commands
+  Cypress.Commands.overwrite('eyesCheckWindow', (eyesCheckWindow, name) => {
+    cy.log(`👀 skipping screenshot **${name}**`)
+  })
+}
