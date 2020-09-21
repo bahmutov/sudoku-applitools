c432554fb90f295a79fa88fe180eaf0ddf96ccc9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-21 12:49:11 -0400

    disable component tests for now

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -8,6 +8,8 @@ describe('Sudoku', () => {
     cy.contains('.status__time', '00:00')
     cy.contains('.status__difficulty-select', 'Easy')
 
+    cy.eyesCheckWindow({ tag: 'App' })
+
     // ignore the contents of the board's cells
     // const oneToNine = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     // cy.eyesCheckWindow({
